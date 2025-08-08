fafd46a02a87e1bc80352e595639124e50d1a964
Shourya <shouryasarkar001@gmail.com>
2025-08-08 16:00:58 +0530

    feat: adds the user login route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -43,6 +43,38 @@ router.post("/register", (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {});
+// login an existing user /auth/login
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(404).send({ message: "Empty username or password" });
+  }
+
+  try {
+    const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`);
+
+    const user = getUser.get(username);
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    const isValidPassword = bcrypt.compareSync(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401).send({ message: "Invalid Password" });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+
+    res.json({ token });
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(503);
+  }
+});
 
 export default router;
